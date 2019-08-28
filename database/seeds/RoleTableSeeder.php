c576c474854ba2d845b79eba010fa47d24621cff
Cesar Garcia <isc.cesar.sistemas@gmail.com>
2019-08-27 22:12:43 -0500

    Se hicieron cambios para mostrar el login y poder ingresar a la pagina por medio de roles se modificaron los controladores homecontroller y los modelos role y user en los seeds el de database y role

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'admin';
+        $role->description = 'Administrador';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'user';
+        $role->description = 'Usuario';
+        $role->save();
+    }
+}
